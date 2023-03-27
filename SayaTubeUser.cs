dccd92026da57e44dcb1fd9f7c9e1ecf18637f05
qibarli <barliant.rifqi20@gmail.com>
2023-03-27 07:01:54 +0700

    pertama

---
+++ SayaTubeUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_MOD6_1302210125
+{
+    internal class Class1
+    {
+    }
+}
