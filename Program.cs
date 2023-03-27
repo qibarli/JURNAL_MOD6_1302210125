29936abd9134c0d20eed331f184c8a720d090bca
qibarli <barliant.rifqi20@gmail.com>
2023-03-27 08:37:49 +0700

    penambahan design by contract pada semua kelas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-asdasdasasdasdasdas
+﻿using System;
+
+namespace Jurnal_MOD6_1302210125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo vid1 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid2 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid3 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid4 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid5 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid6 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid7 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid8 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid9 = new SayaTubeVideo("Asik Asik");
+            SayaTubeVideo vid10 = new SayaTubeVideo("Asik Asik");
+
+            SayaTubeUser user = new SayaTubeUser("MochamadRifqiBarliant");
+            user.AddVideo(vid1);
+            user.AddVideo(vid2);
+            user.AddVideo(vid3);
+            user.AddVideo(vid4);
+            user.AddVideo(vid5);
+            user.AddVideo(vid6);
+            user.AddVideo(vid7);
+            user.AddVideo(vid8);
+            user.AddVideo(vid9);
+            user.AddVideo(vid10);
+
+            user.PrintAllVideoPlaycount();
+        }
+    }
+}
--- SayaTubeUser.cs
+++ SayaTubeUser.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace Jurnal_MOD6_1302210125
 {
-    internal class Class1
+    class SayaTubeUser
     {
+        private int id;
+        private List<SayaTubeVideo> uploadedVideos;
+        private String Username;
+
+        public SayaTubeUser(String user)
+        {
+            this.Username = user;
+            this.uploadedVideos = new List<SayaTubeVideo>();
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int total = 0;
+            foreach (SayaTubeVideo hai in uploadedVideos);
+            total += uploadedVideos.Count;
+            return total;
+
+        }
+
+        public void AddVideo(SayaTubeVideo video)
+        {
+            uploadedVideos.Add(video);
+        }
+
+        public void PrintAllVideoPlaycount()
+        {
+            Console.WriteLine("Username : " + Username);
+            for (int i = 0; i < uploadedVideos.Count; i++)
+            {
+                Console.WriteLine($"Video {i+1} Judul {uploadedVideos[i].GetTitle()}", i+1);
+            }
+        }
     }
+
 }
